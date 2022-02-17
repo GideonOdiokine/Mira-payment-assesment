b42b9345dfd6c9d0459e4c40072cedf3e6c0eda8
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-02-17 20:40:15 +0100

    added Json server

--- src/App.js
+++ src/App.js
@@ -1,110 +1,12 @@
-import React, { useState, useEffect } from "react";
-import {
-  MenuItem,
-  FormControl,
-  Select,
-  Card,
-  CardContent,
-} from "@material-ui/core";
-import "./App.css";
-import InfoBoxs from "./components/InfoBoxs";
-import Map from "./components/Map";
-import { Table } from "./components/Table";
-import { sortData, prettyPrintStat } from "./components/utili";
-import LineGraph from "./components/LineGraph";
-import "leaflet/dist/leaflet.css"
+import React from 'react';
 
 
-const App = () => {
-  const [countries, setCountries] = useState([]);
-  const [selected, setSelected] = useState("worldwide");
-  const [countryInfo, setCountryInfo] = useState({})
-  const [tableData, setTableData] = useState([])
-  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
-  const [mapZoom, setMapZoom] = useState(3);
-  const [mapCountries, setMapCountries] = useState([])
-  const [casesType, setCaseType] = useState("cases")
-
-
-  useEffect(() => {
-    fetch("https://disease.sh/v3/covid-19/all")
-      .then((response) => response.json())
-      .then((data) => {
-        setCountryInfo(data)
-      })
-  }, [])
-
-  useEffect(() => {
-    const getCountriesData = async () => {
-      await fetch("https://disease.sh/v3/covid-19/countries")
-        .then((response) => response.json())
-        .then((data) => {
-          const countries = data.map((country) => ({
-            name: country.country,
-            value: country.countryInfo.iso2,
-          }));
-          const sortedData = sortData(data)
-          setCountries(countries);
-          setTableData(sortedData)
-          setMapCountries(data)
-        });
-    };
-    getCountriesData();
-  }, []);
-
-  const onCountryChange = async (event) => {
-    const countryCode = event.target.value;
-    const url =
-      countryCode === "worldwide"
-        ? "https://disease.sh/v3/covid-19/all"
-        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
-
-    await fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        setSelected(countryCode)
-        setCountryInfo(data)
-        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
-        setMapZoom(4);
-      })
-  };
-
+function App(){
   return (
-    <div className="app">
-      <div className="app__left">
-        <div className="app__header ">
-          <h1>COVID_19 TRACKER</h1>
-          <FormControl className="app__dropdown">
-            <Select
-              variant="outlined"
-              value={selected}
-              onChange={onCountryChange}
-            >
-              <MenuItem value="worldwide">WorldWide</MenuItem>
-              {countries.map((country, i) => (
-                <MenuItem key={i} value={country.value}>{country.name}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </div>
-        <div className="app__stats">
-          <InfoBoxs isRed active={casesType === "cases"} onClick={(e) => setCaseType('cases')} title="Coronavirus Cases" cases={prettyPrintStat(countryInfo.todayCases)} total={prettyPrintStat(countryInfo.cases)} />
-          <InfoBoxs active={casesType === "recovered"} onClick={(e) => setCaseType("recovered")} title="Recovered" cases={prettyPrintStat(countryInfo.todayRecovered)} total={prettyPrintStat(countryInfo.recovered)} />
-          <InfoBoxs isRed active={casesType === "deaths"} onClick={(e) => setCaseType("deaths")} title="Deaths" cases={prettyPrintStat(countryInfo.todayDeaths)} total={prettyPrintStat(countryInfo.deaths)} />
-        </div>
-
-        <Map casesType={casesType} center={mapCenter} zoom={mapZoom} countries={mapCountries} />
-      </div>
-      <Card className="app__right">
-        <CardContent>
-          <h3>Live cases by country</h3>
-          <Table countries={tableData} />
-          <h3>Worldwide new {casesType}</h3>
-          <LineGraph casesType={casesType} />
-        </CardContent>
-      </Card>
-    </div>
-  );
-};
+    <>
+      <h2>App </h2>
+    </>
+  )
+}
 
 export default App;
