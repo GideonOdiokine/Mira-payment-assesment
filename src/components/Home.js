2b27bc2ec74b2aee36a93a0143041229b9d1fc6b
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-02-17 23:36:45 +0100

    added customHook to fetch dynamic data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import BlogList from "./BlogList";
 import "./styles/Home.css";
+import useFetch from "../customHook/useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [Loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const {
+    data: blogs,
+    setData,
+    loading,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   const handleDelete = (id) => {
     if (window.confirm("Are You Sure?")) {
       const newBlog = blogs.filter((blog) => blog.id !== id);
-      setBlogs(newBlog);
+      setData(newBlog);
     }
   };
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("Could not fetch the data for that resource");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setLoading(false);
-        setError(err.message);
-      });
-  }, []);
 
   return (
     <div className="home">
       {error && <h4>{error}</h4>}
-      {Loading && <h3>Loading....</h3>}
+      {loading && <h3>Loading....</h3>}
       {blogs && <BlogList blogs={blogs} handleDelete={handleDelete} />}
     </div>
   );
