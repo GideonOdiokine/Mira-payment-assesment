dd23d8f70bf739177c69fd0ad3f7acefe5be1161
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-02-20 22:36:10 +0100

    imported Link to  blogDetails for routing

--- src/views/BlogDetails.js
+++ src/views/BlogDetails.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import useFetch from "../customHook/useFetch";
 
 function BlogDetails() {
@@ -11,7 +11,7 @@ function BlogDetails() {
   } = useFetch(`http://localhost:8000/blogs/${id}`);
   return (
     <div className="blog-details">
-      <h3>BlogDetails {id}</h3>
+      <h3>Details of the blog {id}</h3>
       {loading && <div>Loading </div>}
       {error && <div>Error </div>}
       {blog && (
@@ -21,6 +21,20 @@ function BlogDetails() {
           <div>{blog.body}</div>
         </article>
       )}
+      <Link
+        style={{
+          textDecoration: "none",
+          background: "#f1356d",
+          color: "#fff",
+          padding: "10px",
+        }}
+        to={{
+          pathname: "/edit",
+          state: { blog: blog },
+        }}
+      >
+        Edit blog
+      </Link>
     </div>
   );
 }
